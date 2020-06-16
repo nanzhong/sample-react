29fe8798efe3670b9f5deb343cb0fe1721822de5
Nan Zhong <me@nanzho.ng>
2020-06-16 10:57:31 -0400

    Modify content for branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Changed!
+        Test Branch!
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
