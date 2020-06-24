917710019ce67d7af35458420c524227ac24f512
Nan Zhong <nzhong@digitalocean.com>
2020-06-24 07:32:42 -0400

    Change 2!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Change 1!
+        Change 2!
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
